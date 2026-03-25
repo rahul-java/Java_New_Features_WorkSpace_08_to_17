3d056ffb4d2b88252f4e8cbed176e5e1f87c69d8
Rahul Kumar Pandey <rp201291.java@gmail.com>
2026-03-25 21:27:51 +0530

    Lambda Exception

--- Java_New_Features_WorkSpace_08_to_17/src/java_new_features/java_08/LambdaExceptionDemo.java
+++ Java_New_Features_WorkSpace_08_to_17/src/java_new_features/java_08/LambdaExceptionDemo.java
@@ -1,10 +1,29 @@
 package java_new_features.java_08;
 
+interface DoubleArrayInterface{
+	double calculate(double[] arr) throws EmptyArrayException;
+}
+class EmptyArrayException extends Exception{
+	public EmptyArrayException() {
+		super("Array is Empty...");
+	}
+}
 public class LambdaExceptionDemo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws EmptyArrayException {
+		
+		double[] dArr= {10.0,20.0,30.0,40.0,50.0};
+		DoubleArrayInterface avg=(arr)->{
+			double sum=0;
+			if(arr.length==0)
+				throw new EmptyArrayException();
+			for(int i=0;i<arr.length;i++)
+				sum+=arr[i];
+			return sum/arr.length;
+		};
+		
+		System.out.println("The Average of double Array elements is : "+avg.calculate(dArr));
+		System.out.println("The Average of double Array elements is : "+avg.calculate(new double[0]));
 	}
 
 }
